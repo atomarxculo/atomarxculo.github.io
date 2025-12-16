0ac1e3b152249cfbadb8211bf8527c2f6fbd7592
Samuel Hernández <samuel.hernandez@gmx.es>
2025-12-16 20:02:57 +0100

    Configurar test

--- tools/test.sh
+++ tools/test.sh
@@ -62,8 +62,7 @@ main() {
 
   # test
   bundle exec htmlproofer "$SITE_DIR" \
-    --disable-external \
-    --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
+    --ignore-urls ".*"
 }
 
 while (($#)); do
